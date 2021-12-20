3d3b5868919303062b53731c23e69837809b0383
Paul Su <paul.su@ohbottech.com>
2021-12-20 15:25:46 +0800

    init project

---
+++ index.js
@@ -0,0 +1,8 @@
+const base = require('./base.eslint.config.js')
+const vue2 = require('./vue2.eslint.config')
+
+module.exports = {
+    configs: {
+        vue2: require('./vue2.eslint.config'),
+    },
+}
---
+++ vue2.eslint.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+    env: {
+        node: true
+    },
+    extends: [
+        require.resolve('./base.eslint.config.js'),
+        'plugin:vue/essential',
+    ],
+    rules: {
+        'vue/max-attributes-per-line': [1, {
+            singleline: 3,
+            multiline: {
+                max: 1,
+                allowFirstLine: false
+            }
+        }],
+        'no-empty-pattern': 'warn',
+        quotes: ['warn', 'single'],
+        'no-unused-vars': 'warn',
+        indent: ['warn', 2],
+        'comma-spacing': ['warn'],
+        'comma-dangle': ['warn', 'always-multiline'],
+        'key-spacing': ['warn', { beforeColon: false }],
+        'keyword-spacing': ['warn'],
+        'space-in-parens': ['warn'],
+        'space-before-function-paren': ['warn', 'always'],
+        'space-before-blocks': ['warn', 'always'],
+        'object-curly-spacing': ['warn', 'always'],
+        'no-multi-spaces': 'warn',
+        'arrow-spacing': 'warn',
+        'space-infix-ops': 'warn',
+        'vue/html-closing-bracket-spacing': ['warn'],
+        'vue/html-indent': ['warn', 2],
+        'vue/attributes-order': 'warn',
+        'vue/order-in-components': 'warn',
+        'vue/html-closing-bracket-newline': 'warn',
+        'vue/html-self-closing': 'warn',
+        'vue/no-multi-spaces': ['warn', {
+            ignoreProperties: false
+        }],
+        'vue/multiline-html-element-content-newline': 'warn',
+        'vue/mustache-interpolation-spacing': 'warn',
+        'no-console': 'warn',
+        'no-debugger': 'warn',
+        'semi': ['warn', 'never'],
+        'quote-props': ['warn', 'as-needed'],
+    }
+}
