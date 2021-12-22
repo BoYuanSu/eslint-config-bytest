5c4d195dc01f84573d86a4eb392b966e2f19a13c
Paul Su <paul.su@ohbottech.com>
2021-12-22 09:10:26 +0800

    v1.0.7

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const base = require('./base.eslint.config.js')
-const vue2 = require('./vue2.eslint.config')
+// const base = require('./base.eslint.config.js')
+// const vue2 = require('./vue2.eslint.config')
 
 module.exports = {
     configs: {
