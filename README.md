68e49575db27d593f985456ec8cf1ab9498d5e6b
Paul Su <paul.su@ohbottech.com>
2021-12-22 13:46:05 +0800

    v1.0.21

---
+++ README.md
@@ -0,0 +1,5 @@
+# my custom eslint rule base on vue2/vue3
+
+## Usage
+
+yarn add -D eslint eslint-plugin-standard eslint-plugin-vue eslint-plugin-node eslint-plugin-import eslint-config-bytest
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // console.log('🚀 ~ require(./lib/vue2),', require('./lib/base'),)
 module.exports = {
-  configs: {
-    base: require('./lib/base'),
-    vue2: require('./lib/vue2'),
-    vue3: require('./lib/vue3'),
-  }
+    configs: {
+        base: require('./lib/base'),
+        vue2: require('./lib/vue2-essential-base'),
+        base: require('./lib/vue2-recommended-base'),
+        vue3: require('./lib/vue3-essential-base'),
+        vue3: require('./lib/vue3-recommended-base'),
+    },
 }
